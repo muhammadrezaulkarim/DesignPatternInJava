72b2e4eb0686e88999c42833d82c5808fb56bf8a
Muhammad Rezaul Karim <33606386+muhammadrezaulkarim@users.noreply.github.com>
2018-12-23 16:23:44 -0700

    Create BridgePatternTestClass.java

---
+++ DesignPatternWorkSpace/DesignPatterns/src/com/pattern/example/bridge/BridgePatternTestClass.java
@@ -0,0 +1,16 @@
+package com.pattern.example.bridge;
+
+
+public class Client 
+{
+  public static void main(String[] args) 
+  {
+    //Create an appropriate implementer object based on the need
+    MessageLogger logger = new FileLogger();
+    
+    //Choose required interface object and configure it with the above implementer object
+    Message msg = new EncryptedMessage(logger);
+    
+    msg.log("Test Message");
+  }
+}
