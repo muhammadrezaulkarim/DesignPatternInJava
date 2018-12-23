45d911f57b9d856c955b630ed6f3ac16d0c12585
Muhammad Rezaul Karim <33606386+muhammadrezaulkarim@users.noreply.github.com>
2018-12-23 14:51:01 -0700

    Update FileLogger.java

--- DesignPatternWorkSpace/DesignPatterns/src/com/pattern/example/factory/FileLogger.java
+++ DesignPatternWorkSpace/DesignPatterns/src/com/pattern/example/factory/FileLogger.java
@@ -1,6 +1,8 @@
 package com.pattern.example.factory;
 
-import Utility.FileUtil;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 
 
 public class FileLogger implements Logger
@@ -8,8 +10,30 @@ public class FileLogger implements Logger
 	
 	public void log(String msg) 
 	{
-		FileUtil futil = new FileUtil();
-		futil.writeToFile("log.txt", msg, true, true);
+	        PrintWriter printer=null;
+		File output=null ;
+		
+		try 
+		{
+		    output = new File("log.txt");
+		    printer = new PrintWriter(output);
+		}
+		catch (FileNotFoundException e1) 
+		{
+		   e1.printStackTrace();
+		}
+		
+	        try 
+                {
+                   printer.write(msg + System.getProperty("line.separator"));
+		} 
+                catch (Exception e) 
+		{
+		   e.printStackTrace();
+		}
+	        
+		printer.flush();
+		 
 	}
 
 }
