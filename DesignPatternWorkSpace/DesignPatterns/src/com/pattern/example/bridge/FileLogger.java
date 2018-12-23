93152132a8b11ec327cf0f28ec8d283eee029608
Muhammad Rezaul Karim <33606386+muhammadrezaulkarim@users.noreply.github.com>
2018-12-23 14:27:20 -0700

    Update FileLogger.java

--- DesignPatternWorkSpace/DesignPatterns/src/com/pattern/example/bridge/FileLogger.java
+++ DesignPatternWorkSpace/DesignPatterns/src/com/pattern/example/bridge/FileLogger.java
@@ -6,7 +6,7 @@ public class FileLogger implements MessageLogger
   public void logMsg(String msg) 
   {
     FileUtil futil = new FileUtil();
-    futil.writeToFile("log.txt”,msg, true, true);
+    futil.writeToFile("log.txt",msg, true, true);
   }
 
 }
