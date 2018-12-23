c4a759f9afb35b9bb21f34409d55ec49b0d6cc4e
Muhammad Rezaul Karim <33606386+muhammadrezaulkarim@users.noreply.github.com>
2018-12-23 15:15:12 -0700

    Update FactoryPatternTestClass.java

--- DesignPatternWorkSpace/DesignPatterns/src/com/pattern/example/factory/FactoryPatternTestClass.java
+++ DesignPatternWorkSpace/DesignPatterns/src/com/pattern/example/factory/FactoryPatternTestClass.java
@@ -1,13 +1,14 @@
 package com.pattern.example.factory;
 
-import Utility.Logger;
-
 public class FactoryPatternTestClass 
 {
 	public static void main(String [] args[])
 	{
+		 
 		 LoggerFactory factory=new LoggerFactory();
-		 Logger logger=factory.getLogger();
+		 
+		 # Returns a object based on current configuration
+		 MessageLogger logger=factory.getLogger();
 		 logger.log("A message to log.");
 	}
 }
