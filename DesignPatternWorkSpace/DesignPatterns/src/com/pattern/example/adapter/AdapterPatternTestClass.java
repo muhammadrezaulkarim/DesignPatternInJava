33d5b5bf9f2e02992b86234e87ed268d80a3fda6
Muhammad Rezaul Karim <33606386+muhammadrezaulkarim@users.noreply.github.com>
2018-12-23 23:23:16 -0700

    Update AdapterPatternTestClass.java

--- DesignPatternWorkSpace/DesignPatterns/src/com/pattern/example/adapter/AdapterPatternTestClass.java
+++ DesignPatternWorkSpace/DesignPatterns/src/com/pattern/example/adapter/AdapterPatternTestClass.java
@@ -4,9 +4,11 @@ public class AdapterPatternTestClass
 {
   public static void main(String[] args) 
   {
+    // Prepare an incompatible class object
     BugattiVeyron luxuryCar = new  BugattiVeyron();
     luxuryCar.setSpeed(270.0);
     
+    // Prepare a compatible class object feeding the incompatible class object
     MovableAdapterImpl obj = new MovableAdapterImpl(luxuryCar);
     System.out.println(obj.getSpeed());
   }
