5433d61d2474b4be5619f90a5d374705879c326e
Muhammad Rezaul Karim <33606386+muhammadrezaulkarim@users.noreply.github.com>
2018-12-23 17:27:58 -0700

    Create TextFile.java

---
+++ DesignPatternWorkSpace/DesignPatterns/src/com/pattern/example/command/TextFile.java
@@ -0,0 +1,16 @@
+public class TextFile {
+     
+    private String name;
+     
+    // constructor
+     
+    public String open() {
+        return "Opening file " + name;
+    }
+     
+    public String save() {  
+        return "Saving file " + name;
+    }
+     
+    // additional text file methods (editing, writing, copying, pasting)
+}
