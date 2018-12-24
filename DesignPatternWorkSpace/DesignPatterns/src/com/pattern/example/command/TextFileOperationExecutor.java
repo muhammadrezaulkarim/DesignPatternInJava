7b52bd7beeee4604b28e845ca450be8a54f8fd8d
Muhammad Rezaul Karim <33606386+muhammadrezaulkarim@users.noreply.github.com>
2018-12-23 17:29:50 -0700

    Create TextFileOperationExecutor.java

---
+++ DesignPatternWorkSpace/DesignPatterns/src/com/pattern/example/command/TextFileOperationExecutor.java
@@ -0,0 +1,12 @@
+package com.pattern.example.command
+
+public class TextFileOperationExecutor {
+     
+    private final List<TextFileOperation> textFileOperations
+     = new ArrayList<>();
+     
+    public String executeOperation(TextFileOperation textFileOperation) {
+        textFileOperations.add(textFileOperation);
+        return textFileOperation.execute();
+    }
+}
