5535524bb6e5ab5d4d3e2e3c4cae861599fabcd8
Muhammad Rezaul Karim <33606386+muhammadrezaulkarim@users.noreply.github.com>
2018-12-23 19:31:45 -0700

    Create ExpensiveObjectProxy.java

---
+++ DesignPatternWorkSpace/DesignPatterns/src/com/pattern/example/proxy/virtualproxy/ExpensiveObjectProxy.java
@@ -0,0 +1,13 @@
+package com.pattern.example.proxy.virtualproxy;
+
+public class ExpensiveObjectProxy implements ExpensiveObject {
+    private static ExpensiveObject object;
+ 
+    @Override
+    public void process() {
+        if (object == null) {
+            object = new ExpensiveObjectImpl();
+        }
+        object.process();
+    }
+}
