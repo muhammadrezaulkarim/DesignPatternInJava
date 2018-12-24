74f485c906c714a81dc6ca92678249462a0ed03b
Muhammad Rezaul Karim <33606386+muhammadrezaulkarim@users.noreply.github.com>
2018-12-23 17:44:24 -0700

    Update CommandPatternTestClass.java

--- DesignPatternWorkSpace/DesignPatterns/src/com/pattern/example/command/CommandPatternTestClass.java
+++ DesignPatternWorkSpace/DesignPatterns/src/com/pattern/example/command/CommandPatternTestClass.java
@@ -8,7 +8,7 @@ public class CommandPatternTestClass
         TextFileOperationExecutor textFileOperationExecutor = new TextFileOperationExecutor();
 
         // execute open command (i.e OpenTextFileOperation) on the receiver object (i.e. Text File) using the invoker 
-        (i.e. TextFileOperationExecutor class)
+        // (i.e. TextFileOperationExecutor class)
         textFileOperationExecutor.executeOperation(new OpenTextFileOperation(new TextFile("file1.txt"))));
         
         // execute save command on the receiver object (e.g. Text File) using the invoker (i.e. TextFileOperationExecutor class)
