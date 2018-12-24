e23b4280f63565a19e9eb18487db11a84391d9e0
Muhammad Rezaul Karim <33606386+muhammadrezaulkarim@users.noreply.github.com>
2018-12-23 17:41:40 -0700

    Update OpenTextFileOperation.java

--- DesignPatternWorkSpace/DesignPatterns/src/com/pattern/example/command/OpenTextFileOperation.java
+++ DesignPatternWorkSpace/DesignPatterns/src/com/pattern/example/command/OpenTextFileOperation.java
@@ -6,6 +6,10 @@ public class OpenTextFileOperation implements TextFileOperation {
     private TextFile textFile;
      
     // constructors
+    public OpenTextFileOperation(TextFile textFile)
+    {
+      this.textFile =  textFile;
+    }
      
     @Override
     public String execute() {
